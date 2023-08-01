ec67d344e9c9cd95f109cca76e2978675f0475e7
Vrishali2212 <vrish2212@gmail.com>
2023-07-31 17:23:29 -0700

    Change in constans file

--- src/test/java/com/APITestingExamination/Utilities/Constants.java
+++ src/test/java/com/APITestingExamination/Utilities/Constants.java
@@ -2,6 +2,8 @@ package com.APITestingExamination.Utilities;
 
 public class Constants {
 	
+	// Adding some line 
+	
 	public static final String CUR_DIR=System.getProperty("user.dir") ; 
 	public static final String PROPERTIES_FILE=CUR_DIR+"/src/test/resources/propertyFile.properties";
 	public static final String EXCELFILEPATH=CUR_DIR+"/src/test/resources/NewData.xlsx";
